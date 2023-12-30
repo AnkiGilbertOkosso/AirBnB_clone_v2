b8a49a0e0387e70c06072e3fbf9b8e56590e686a
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2023-12-30 08:35:02 +0000

    updates

--- models/state.py
+++ models/state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-""" State Module for HBNB project"""
-
-from models.base_model import BaseModel, Base
+"""This is the state class"""
 from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 import models
@@ -12,23 +11,25 @@ import shlex
 
 class State(BaseModel, Base):
     """This is the class for State
+    Attributes:
+        name: input name
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City",
-            cascade='all, delete, delete-orphan', backref="state")
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
     @property
     def cities(self):
-        kw = models.storage.all()
-        ls = []
-        res = []
-        for key in kw:
+        diction = models.storage.all()
+        lists = []
+        result = []
+        for key in diction:
             city = key.replace('.', ' ')
             city = shlex.split(city)
             if (city[0] == 'City'):
-                ls.append(kw[key])
-        for i in ls:
-            if (i.state_id == self.id):
-                res.append(i)
-        return (res)
+                lists.append(diction[key])
+        for n in lists:
+            if (n.state_id == self.id):
+                result.append(n)
+        return (result)
